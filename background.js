30deb8e47de10dd48117a2801d02d4e72f4274bc
Mohamed <7mdiano@gmail.com>
2023-01-27 00:55:05 +0200

    update

--- background.js
+++ background.js
@@ -9,35 +9,38 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                     var groups = [];
                     for (var i = 0; i < radioButtons.length; i++) {
                         var group = radioButtons[i].getAttribute('name');
-                        if (groups.indexOf(group) === -1) {
-                            groups.push(group);
-                        }
+                        var index = '${request.selectedValue}';
+                        group[index-1].checked = true;
+                        // if (groups.indexOf(group) === -1) {
+                        //     groups.push(group);
+                        // }
                     }
-                    const index = '${request.selectedValue}';
-                    // check the selected radio button
-                    for (var i = 0; i < groups.length; i++) {
-                        var group = groups[i];
-                        group[index].checked = true;
-                    }
+                    var index = '${request.selectedValue}';
+                    // alert(index);
+                    // // check the selected radio button
+                    // for (var i = 0; i < groups.length; i++) {
+                    //     var group = groups[i];
+                    //     group[index-1].checked = true;
+                    // }
                 `,
             });
         });
     }
 });
 
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.selectedValue) {
-//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//             chrome.tabs.executeScript(tabs[0].id, {
-//                 code: `
-//                     var radioButtons = document.querySelectorAll('input[type="radio"]');
-//                     for (var i = 0; i < radioButtons.length; i++) {
-//                         if (radioButtons[i].value == "${request.selectedValue}") {
-//                             radioButtons[i].checked = true;
-//                         }
-//                     }
-//                 `,
-//             });
-//         });
-//     }
-// });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.selectedValue) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.executeScript(tabs[0].id, {
+                code: `
+                    var radioButtons = document.querySelectorAll('input[type="radio"]');
+                    for (var i = 0; i < radioButtons.length; i++) {
+                        if (radioButtons[i].value == "${request.selectedValue}") {
+                            radioButtons[i].checked = true;
+                        }
+                    }
+                `,
+            });
+        });
+    }
+});
